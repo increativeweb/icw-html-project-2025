0b04e21a4870d699f404c7de70216b4569543f04
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2025-06-21 21:47:08 +0530

    Update Footer in VKSound HTML

--- vk-sound/html/assets/js/main.js
+++ vk-sound/html/assets/js/main.js
@@ -35,6 +35,44 @@ jQuery(document).ready(function($) {
         $(this).toggleClass('is-active');
         $(this).parent().find('.sub-menu').first().toggle(300);
     });
+
+     if ($(".icw-progress-goto").length > 0) {
+        var progressPath = document.querySelector('.icw-progress-goto path');
+        var pathLength = progressPath.getTotalLength();
+    
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+    
+        var updateProgress = function() {
+            var scroll = $(window).scrollTop();
+            var height = $(document).height() - $(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        }
+    
+        updateProgress();
+        $(window).scroll(updateProgress);
+    
+        var offset = 200;
+        var duration = 550;
+    
+        jQuery(window).on('scroll', function() {
+            if(jQuery(this).scrollTop() > offset) {
+                jQuery('.icw-progress-goto').addClass('active-progress');
+            } else {
+                jQuery('.icw-progress-goto').removeClass('active-progress');
+            }
+        });
+    
+        jQuery('.icw-progress-goto').on('click', function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        });
+    }
 });
 // Splide Slider
 document.addEventListener('DOMContentLoaded', function () {
