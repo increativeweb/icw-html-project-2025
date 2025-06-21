b7039d3909162c005305f2350978beefbb7baafc
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2025-06-21 18:40:53 +0530

    Update all slider in splide slider in VKSound HTML

--- vk-sound/html/assets/js/main.js
+++ vk-sound/html/assets/js/main.js
@@ -35,80 +35,17 @@ jQuery(document).ready(function($) {
         $(this).toggleClass('is-active');
         $(this).parent().find('.sub-menu').first().toggle(300);
     });
-
 });
-if ($('.hero-slider').length) {
-    var swiper = new Swiper('.hero-slider', {
-        speed: 600,
-        parallax: true,
-        pagination: {
-            el: '.swiper-pagination',
-            dynamicBullets: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-}
-if ($('.logo-slider').length) {
-    var swiper = new Swiper('.logo-slider', {
-        slidesPerView: 4,
-        loop: true,
-        centeredSlides: true,
-        spaceBetween: 30,
-        speed: 2000,
-        autoplay: {
-            delay: 0,
-            enabled: true,
-        },
-        breakpoints: {
-            992: {
-                slidesPerView: 5,
-                spaceBetween: 30
-            },
-            1200: {
-                slidesPerView: 10,
-                spaceBetween: 50
-            },
-        }
-    });
-}
-if ($('.card-slider').length) {
-    var swiper = new Swiper('.card-slider', {
-        slidesPerView: 1.25,
-        loop: true,
-        spaceBetween: 20,
-        autoplay: true,
-        speed: 2000,
-        autoplay: {
-            delay: 1000,
-            enabled: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            dynamicBullets: true,
-        },
-        breakpoints: {
-            767: {
-                slidesPerView: 2.25,
-                spaceBetween: 20,
-            },
-            992: {
-                slidesPerView: 3,
-                spaceBetween: 30
-            },
-            1200: {
-                slidesPerView: 4,
-                spaceBetween: 30
-            },
-        }
-    });
-}
+// Splide Slider
+document.addEventListener('DOMContentLoaded', function () {
+    var splides = document.querySelectorAll('.splide:not(.splide-js)');
+    if (splides.length > 0) {
+        splides.forEach(function (el) {
+            new Splide(el).mount();
+            el.classList.add('icw_splide-with-data');
+        });
+    }
+})
 // Counter
 if ($('.counter').length) {
     let options = {
