fb9cb4945e859c78045f55289cbc1957a20b308f
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-05-02 00:57:52 +0530

    Update Header & Footer For New EVA HTML

---
+++ evainc.com/html/assets/js/main.js
@@ -0,0 +1,138 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+var $ = jQuery.noConflict();
+
+jQuery(document).ready(function ($) {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+    if ($('.main-header').length) {
+
+        if (jQuery(this).scrollTop() > 50) {
+            $(".main-header").addClass("fixed-header");
+        } else {
+            $(".main-header").removeClass("fixed-header");
+        }
+
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 50) {
+                $(".main-header").addClass("fixed-header");
+            } else {
+                $(".main-header").removeClass("fixed-header");
+            }
+        });
+
+        $('.navbar-toggler').on('click', function () {
+            $(".main-header").toggleClass('is-visible');
+            $('body').toggleClass('overflow-hidden');
+            $(this).toggleClass('is-visible');
+        });
+        if ($('li.menu-item-has-children').length) {
+            $("li.menu-item-has-children > a").append('<i class="arrow"></i>');
+        }
+        $('.menu-item-has-children .arrow').on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            const $li = $(this).closest('.menu-item-has-children');
+            const $submenu = $li.children('.sub-menu');
+
+            $(this).toggleClass('is-active');
+            $submenu.stop(true, true).slideToggle(300);
+        });
+
+        var $menuItems = $('.menu-item-has-children.mega-menu');
+
+        $menuItems.each(function () {
+
+            var $item = $(this);
+            var $submenu = $item.find('.sub-menu').first();
+
+            if (!$submenu.length) return;
+
+            // Function to set height
+            function setHeight() {
+                var height = $submenu[0].scrollHeight + 2;
+                $item.css('--submenu-height', height + 'px');
+            }
+
+            let hoverTimeout;
+
+            $item.on('mouseenter', function () {
+                if ($(window).width() >= 1200) {
+                    clearTimeout(hoverTimeout);
+                    setHeight();
+                }
+            });
+
+            $item.on('mouseleave', function () {
+                if ($(window).width() >= 1200) {
+                    hoverTimeout = setTimeout(() => {
+                        $item.css('--submenu-height', '0px');
+                    }, 100); // small delay prevents flicker
+                }
+            });
+        });
+
+        // Resize fix
+        $(window).on('resize', function () {
+
+            $('.menu-item-has-children.is-open').each(function () {
+            var $item = $(this);
+            var $submenu = $item.find('.sub-menu').first();
+
+            if ($submenu.length) {
+                $item.css('--submenu-height', $submenu[0].scrollHeight + 'px');
+            }
+            });
+
+        });
+    }
+
+
+    if ($(".icw-progress-goto").length > 0) {
+        var progressPath = document.querySelector('.icw-progress-goto path');
+        var pathLength = progressPath.getTotalLength();
+
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+
+        var updateProgress = function () {
+            var scroll = $(window).scrollTop();
+            var height = $(document).height() - $(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        }
+
+        updateProgress();
+        $(window).scroll(updateProgress);
+
+        var offset = 200;
+        var duration = 550;
+
+        jQuery(window).on('scroll', function () {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.icw-progress-goto').addClass('active-progress');
+            } else {
+                jQuery('.icw-progress-goto').removeClass('active-progress');
+            }
+        });
+
+        jQuery('.icw-progress-goto').on('click', function (event) {
+            event.preventDefault();
+            jQuery('html, body').animate({ scrollTop: 0 }, duration);
+            return false;
+        });
+    }
+
+});
+
+// Splide Slider
+if (jQuery('.splide:not(.splide-js)').length) {
+    jQuery('.splide:not(.splide-js)').each(function () {
+        new Splide(this).mount();
+        jQuery(this).addClass('icw_splide-with-data'); // Mark as initialized
+    });
+}
