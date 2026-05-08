331e99a3698898b562e8fa468df342680e60a118
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-05-08 19:29:49 +0530

    Update Hero Section, Content Section in EVA HTML

--- evainc.com/html/blocks/hero/script.js
+++ evainc.com/html/blocks/hero/script.js
@@ -5,9 +5,32 @@ jQuery(document).ready(function ($) {
         arrows: false,  
         pagination: true,
         autoplay: true,
-        interval: 4000,
+        interval: 10000,
         pauseOnHover: false,
         pauseOnFocus: false,
     });
+    const $heroSection = $('.hero-section');
+
+    // Update background function
+    function updateHeroBackground() {
+
+        const activeSlide = $('.hero-splide .splide__slide.is-active');
+        const imgSrc = activeSlide.find('.slide-img img').attr('src');
+
+        if (imgSrc) {
+            $heroSection.css({
+                'background-image': 'url(' + imgSrc + ')',
+            });
+        }
+    }
+
+    // On mounted + slide change
+    heroSplide.on('mounted move', function () {
+        updateHeroBackground();
+    });
+
     heroSplide.mount();
+
+    // Initial
+    updateHeroBackground();
 });
