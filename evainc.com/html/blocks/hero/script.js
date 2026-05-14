9e1df643435320308f3aaaeeece241b5d3bbe995
jayesh <jay.wpdemo@gmail.com>
2026-05-14 19:50:13 +0530

    Update Slider Animation & Consulting Details Page in EVA HTML

--- evainc.com/html/blocks/hero/script.js
+++ evainc.com/html/blocks/hero/script.js
@@ -1,46 +1,64 @@
 jQuery(document).ready(function ($) {
-    if($('.hero-section').length) {
+
+    if ($('.hero-section').length) {
+        const $heroSection = $('.hero-section');
+        const $bgLayer = $('<div class="hero-bg-layer"></div>');
+
+        $heroSection.prepend($bgLayer);
         var heroSplide = new Splide('.hero-splide', {
             type: 'fade',
             rewind: true,
-            arrows: false,  
+            arrows: false,
             pagination: true,
             autoplay: true,
             interval: 5000,
+            speed: 1000,
             pauseOnHover: false,
             pauseOnFocus: false,
         });
-        const $heroSection = $('.hero-section');
 
-        // Update background function
+        // Update background image
         function updateHeroBackground() {
+
             const activeSlide = $('.hero-splide .splide__slide.is-active');
             let imgSrc;
-            // Mobile
+
+            // Mobile image
             if ($(window).width() < 575) {
                 imgSrc = activeSlide.find('.slide-img .mobile-img').attr('src');
-            } 
-            // Desktop
+            }
+            // Desktop image
             else {
                 imgSrc = activeSlide.find('.slide-img img:not(.mobile-img)').attr('src');
             }
+
             if (imgSrc) {
-                $heroSection.css({
-                    'background-image': 'url(' + imgSrc + ')',
-                });
+                // Reset scale animation
+                setTimeout(function () {
+                    $heroSection.removeClass('bg-animate');
+                }, 3000);
+
+                $heroSection.css('--hero-bg', `url(${imgSrc})`);
+                    $heroSection.addClass('bg-animate');
             }
         }
-        // On mounted + slide change
-        heroSplide.on('mounted move', function () {
+
+        // On slide active
+        heroSplide.on('active', function (slide) {
+            // Remove old animation class
+            $('.hero-section .section-title').removeClass('is-animate');
+            // Add animation to active slide
+            $(slide.slide).find('.section-title').addClass('is-animate');
+
             updateHeroBackground();
         });
+        heroSplide.on('mounted active moved', function () {
+            updateHeroBackground();
+        });
+        // Resize
         $(window).on('resize', function () {
             updateHeroBackground();
         });
-
         heroSplide.mount();
-
-        // Initial
-        updateHeroBackground();
     }
 });
