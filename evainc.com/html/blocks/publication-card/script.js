372264fb981f6c5f8796e66d9a82604124ff5b54
jayesh <jay.wpdemo@gmail.com>
2026-05-23 18:25:11 +0530

    Update Who We Help Page in Final EVA HTML

---
+++ evainc.com/html/blocks/publication-card/script.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function ($) {
+
+    if ($('.relevant-publications-splide').length) {
+
+        var publicationsSplide = new Splide('.relevant-publications-splide', {
+            type: 'slide',
+            rewind: true,
+            arrows: false,
+            pagination: true,
+            perPage: 4,
+            perMove: 1,
+            gap: 30,
+            omitEnd: true,
+            breakpoints: {
+                1200: {
+            		type: 'loop',
+                    perPage: 2,
+					autoWidth: true,
+					// focus: 'center'
+                },
+                767: {
+					perPage: 1,
+                    gap: 20,
+                },
+            }
+        });
+
+        publicationsSplide.on('mounted resize', function () {
+
+            const slideLength = publicationsSplide.length;
+            const isDesktop = window.innerWidth > 1200;
+
+            if (isDesktop && slideLength <= 4) {
+
+                publicationsSplide.options = {
+                    drag: false,
+                    arrows: false,
+                    pagination: false,
+                    autoplay: false,
+					gap: 0,
+                };
+
+                $('.relevant-publications-splide').addClass('slider-disabled');
+
+            } else {
+
+                publicationsSplide.options = {
+                    drag: true,
+                    pagination: true,
+                };
+
+                $('.relevant-publications-splide').removeClass('slider-disabled');
+            }
+
+        });
+
+        publicationsSplide.mount();
+    }
+
+});
