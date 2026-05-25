58895a1c4d03bf12d9d6ce6c455e8e3334028018
jayesh <jay.wpdemo@gmail.com>
2026-05-25 12:59:08 +0530

    Update Arrow in Slider

--- evainc.com/html/blocks/info-card/script.js
+++ evainc.com/html/blocks/info-card/script.js
@@ -1,36 +1,71 @@
+// jQuery(document).ready(function ($) {
+//     if ($('.info-card-splide').length) {
+
+//         var infoCardSplide = new Splide('.info-card-splide', {
+//             type: 'loop',
+//             gap: '30px',
+//             arrows: true,
+//             pagination: false,
+//             autoplay: true,
+//             interval: 5000,
+//             speed: 1000,
+//             perPage: 3,
+//             perMove: 1,
+//             start: 0,
+//             focus: 0,
+//             trimSpace: false,
+//             updateOnMove: true,
+//             breakpoints: {
+//                 1200: {
+//                     perPage: 2,
+//                     autoWidth: true,
+//                     focus: 0,
+//                 },
+//                 767: {
+//                     perPage: 1,
+//                     gap: '20px',
+//                 },
+//             }
+//         });
+//         infoCardSplide.on('move', function () {
+//             $('.info-card-splide').removeClass('hide-prev-clone');
+//         });
+//         infoCardSplide.mount();
+//     }
+
+// });
 jQuery(document).ready(function ($) {
-    if ($('.info-card-splide').length) {
 
-        var infoCardSplide = new Splide('.info-card-splide', {
-            type: 'loop',
-            gap: '30px',
-            arrows: true,
-            pagination: false,
-            autoplay: true,
-            interval: 5000,
-            speed: 1000,
-            perPage: 3,
-            perMove: 1,
-            start: 0,
-            focus: 0,
-            trimSpace: false,
+    const infoCardSplide = new Splide('.info-card-splide', {
+        type: 'loop',
+        gap: '30px',
+        arrows: true,
+        pagination: false,
+        autoplay: true,
+        interval: 10000,
+        speed: 1000,
+        perPage: 3,
+        perMove: 1,
+        focus: 0,
+        trimSpace: false,
+        updateOnMove: true,
+        breakpoints: {
+            1200: {
+                perPage: 2,
+                autoWidth: true,
+            },
+            767: {
+                perPage: 1,
+                gap: '20px',
+            },
+        }
+    });
 
-            breakpoints: {
-                1200: {
-                    perPage: 2,
-                    autoWidth: true,
-                    focus: 0,
-                },
-                767: {
-                    perPage: 1,
-                    gap: '20px',
-                },
-            }
-        });
-        infoCardSplide.on('move', function () {
-            $('.info-card-splide').removeClass('hide-prev-clone');
-        });
-        infoCardSplide.mount();
-    }
+    // Change DURING slide move
+    infoCardSplide.on('move', function (newIndex) {
+        $('.info-card-splide').removeClass('hide-prev-clone');
+    });
+
+    infoCardSplide.mount();
 
 });
