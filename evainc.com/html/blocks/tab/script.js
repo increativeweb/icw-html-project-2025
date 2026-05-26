a0505ef556cf3ba20106bb0c9e6828be2259a4cd
jayesh <jay.wpdemo@gmail.com>
2026-05-26 18:13:19 +0530

    Update Blog Details Page in EVA HTML

--- evainc.com/html/blocks/tab/script.js
+++ evainc.com/html/blocks/tab/script.js
@@ -1,23 +1,26 @@
 // Tab Responsive collapse
-if (jQuery('.tab-content-collapse-title').length) {
-    jQuery(document).on("click", ".tab-content-collapse-title .btn-link-arrow", function () {
+if (jQuery(window).width() < 992) {
+    if (jQuery('.tab-content-collapse-title').length) {
+        jQuery(document).on("click", ".tab-content-collapse-title .btn-link-arrow", function () {
 
-        if ($(this).hasClass("is-open")) {
-            $(this).removeClass("is-open");
-            $(this).parents('.tab-pane').find(".tab-content-collapse-body").stop(true, true).slideUp(300);
-        } else {
-            $('.tab-content-collapse-title .btn-link-arrow').removeClass('is-open');
-            $('.tab-content-collapse-body').slideUp(300);
-            $(this).addClass("is-open");
-            $(this).parents('.tab-pane').find(".tab-content-collapse-body").stop(true, true).slideDown(300); 
-            
-            var collapsetop = $(this).find(".tab-content-collapse-title");
-            $('html, body').animate({
-                scrollTop: collapsetop.offset().top - 500   
-            }, 300); // Smooth scroll to the item
-        }
-        return false;
-    });
+            if ($(this).hasClass("is-open")) {
+                $(this).removeClass("is-open");
+                $(this).parents('.tab-pane').find(".tab-content-collapse-body").stop(true, true).slideUp(300);
+            } else {
+                $('.tab-content-collapse-title .btn-link-arrow').removeClass('is-open');
+                $('.tab-content-collapse-body').slideUp(300);
+                $(this).addClass("is-open");
+                $(this).parents('.tab-pane').find(".tab-content-collapse-body").stop(true, true).slideDown(300); 
+                
+                var collapsetop = $(this).parents('.tab-pane');
+                console.log(collapsetop);
+                $('html, body').animate({
+                    scrollTop: Math.max(collapsetop.offset().top - 120, 0)
+                }, 300);
+            }
+            return false;
+        });
+    }
 }
 
 // Tab Wise add Class infocard
