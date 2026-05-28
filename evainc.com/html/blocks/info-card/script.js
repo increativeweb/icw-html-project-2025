aab37184bd1a81f8c24d734ffe86daa4bfff3db1
jayesh <jay.wpdemo@gmail.com>
2026-05-28 14:20:28 +0530

    Update Arrow & Slider Refresh script in EVA HTML

--- evainc.com/html/blocks/info-card/script.js
+++ evainc.com/html/blocks/info-card/script.js
@@ -1,41 +1,55 @@
 jQuery(document).ready(function ($) {
     if ($('.info-card-splide').length) {
+        let infoCardSplide;
 
-        var infoCardSplide = new Splide('.info-card-splide', {
-            type: 'loop',
-            gap: '30px',
-            arrows: true,
-            pagination: false,
-            autoplay: true,
-            interval: 5000,
-            speed: 1000,
-            perPage: 3,
-            perMove: 1,
-            start: 0,
-            focus: 0,
-            trimSpace: false,
-            updateOnMove: true,
-            breakpoints: {
-                1200: {
-                    perPage: 2,
-                    autoWidth: true,
-                    focus: 0,
-                    arrows: false,
-                    pagination: true,
-                },
-                992: {
-                    padding: { left: 15, right: 15 }
-                },
-                767: {
-                    perPage: 1,
-                    gap: '20px',
-                },
+        function initSplide() {
+
+            if (infoCardSplide) {
+                infoCardSplide.destroy(true);
             }
-        });
-        infoCardSplide.on('move', function () {
-            $('.info-card-splide').removeClass('hide-prev-clone');
-        });
-        infoCardSplide.mount();
-    }
 
+            infoCardSplide = new Splide('.info-card-splide', {
+                type: 'loop',
+                gap: '30px',
+                arrows: true,
+                pagination: false,
+                autoplay: true,
+                interval: 5000,
+                speed: 1000,
+                perPage: 3,
+                perMove: 1,
+                updateOnMove : true,
+                breakpoints: {
+                    1200: {
+                        perPage: 2,
+                        autoWidth: true,
+                        focus: 0,
+                        arrows: false,
+                        pagination: true,
+                    },
+                    992: {
+                        padding: { left: 15, right: 15 }
+                    },
+                    767: {
+                        perPage: 1,
+                        gap: '20px',
+                    },
+                }
+            });
+
+            infoCardSplide.mount();
+        }
+
+        initSplide();
+
+        let resizeTimer;
+
+        $(window).on('resize', function () {
+            clearTimeout(resizeTimer);
+
+            resizeTimer = setTimeout(function () {
+                initSplide();
+            }, 200);
+        });
+    }
 });
