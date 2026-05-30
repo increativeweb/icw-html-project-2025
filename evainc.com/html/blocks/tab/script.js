6359a55db83b8374d67bd7a4119c0094c2e1a0eb
jayesh <jay.wpdemo@gmail.com>
2026-05-30 10:08:28 +0530

    Update Search Page in EVA HTML

--- evainc.com/html/blocks/tab/script.js
+++ evainc.com/html/blocks/tab/script.js
@@ -62,7 +62,7 @@ function initWhoWeHelpAccordion() {
                 $this.addClass('is-open');
                 $body.stop(true, true).slideDown(300);
             });
-        }, 320);
+        }, 300);
     });
     // Initial load
     mobileAccordion();
