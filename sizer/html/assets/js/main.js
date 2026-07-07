0d50e29b2096eea553dd60a06010215a520aaaeb
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-07-07 23:39:43 +0530

    Update Other Section in New Sizer HTML

--- sizer/html/assets/js/main.js
+++ sizer/html/assets/js/main.js
@@ -24,4 +24,90 @@ jQuery(document).ready(function ($) {
             $(this).toggleClass('is-visible');
         });
     }
+    if ($('.icw-progress-goto').length > 0) {
+        var progressPath = document.querySelector('.icw-progress-goto path');
+        var pathLength = progressPath.getTotalLength();
+
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+
+        var updateProgress = function () {
+            var scroll = $(window).scrollTop();
+            var height = $(document).height() - $(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        }
+
+        updateProgress();
+        $(window).scroll(updateProgress);
+
+        var offset = 200;
+        var duration = 550;
+
+        jQuery(window).on('scroll', function () {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.icw-progress-goto').addClass('active-progress');
+            } else {
+                jQuery('.icw-progress-goto').removeClass('active-progress');
+            }
+        });
+
+        jQuery('.icw-progress-goto').on('click', function (event) {
+            event.preventDefault();
+            jQuery('html, body').animate({ scrollTop: 0 }, duration);
+            return false;
+        });
+    }
 });
+if ($('.card-img-splide').length > 0) {
+    $('.card-img-splide').each(function () {
+        var slider = this;
+        var slideCount = $(slider).find('.splide__slide').length;
+        var splide = new Splide(slider, {
+            type: 'loop',
+            perPage: 1,
+            perMove: 1,
+            arrows: true,
+            pagination: true,
+            // autoplay: true,
+            // interval: 5000,
+            // speed: 1000,
+            pauseOnHover: true,
+            pauseOnFocus: false,
+            updateOnMove: true,
+        });
+        if (slideCount <= 1) {
+            splide.mount();
+            splide.destroy(true); 
+        } else {
+            splide.mount();
+        }
+
+    });
+}
+if ($('.media-block.is-video').length > 0) {
+    $('.media-block.is-video').each(function () {
+        const $block = $(this);
+        const $icon = $block.find('.play-icon');
+        const video = $block.find('video').get(0);
+        if (!video) return;
+        const playVideo = () => {
+            video.play().then(() => { $block.addClass('video-playing'); $icon.addClass('d-none'); }).catch(() => {});
+        };
+        const pauseVideo = () => { video.pause(); $block.removeClass('video-playing'); $icon.removeClass('d-none'); };
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => { entry.isIntersecting ? playVideo() : pauseVideo(); });
+        }, {
+            threshold: 0.9
+        });
+        observer.observe(video);
+        $(video).on('click', pauseVideo);
+        $icon.on('click', function (e) {
+            e.stopPropagation();
+            playVideo();
+        });
+    });
+}
