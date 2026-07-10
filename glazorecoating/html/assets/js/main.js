b93ce245aabae4fc2beadbfa61c25f7800b56b50
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-07-10 20:14:26 +0530

    Add Hero Section, Header & Half Footer in New Glazore Coating HTML

---
+++ glazorecoating/html/assets/js/main.js
@@ -0,0 +1,28 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+var $ = jQuery.noConflict();
+
+jQuery(document).ready(function ($) {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+    if ($('.main-header').length) {
+        $('.navbar-toggler').on('click', function () {
+            $(".main-header").toggleClass('is-visible');
+            $('body').toggleClass('overflow-hidden');
+            $(this).toggleClass('is-visible');
+        });
+        if ($('li.menu-item-has-children').length) {
+            $("li.menu-item-has-children > a").append('<i class="arrow"></i>');
+        }
+        $('.menu-item-has-children .arrow').on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            const $li = $(this).closest('.menu-item-has-children');
+            const $submenu = $li.children('.sub-menu');
+
+            $(this).toggleClass('is-active');
+            $submenu.stop(true, true).slideToggle(300);
+        });
+    }
+});
