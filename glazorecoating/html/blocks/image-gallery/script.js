d47d2d0ca67324910424beeedf809ad228d40310
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2026-07-13 23:42:53 +0530

    Update Autoplay Slider Section in New Glazore Coating HTML

---
+++ glazorecoating/html/blocks/image-gallery/script.js
@@ -0,0 +1,23 @@
+const splideConfig = (direction = 'ltr') => ({
+    perPage: 7,
+    autoWidth: true,
+    pagination: false,
+    arrows: false,
+    gap: 15,
+    type: 'loop',
+    focus: 'center',
+    mediaQuery: 'min',
+    direction: direction,
+    autoScroll: {
+        // speed: 1
+    },
+});
+// Left slider
+if (document.querySelector('.image-gallery-splide.is-left')) {
+    new Splide('.image-gallery-splide.is-left', splideConfig()).mount(window.splide.Extensions);
+}
+
+// Right slider
+if (document.querySelector('.image-gallery-splide.is-right')) {
+    new Splide('.image-gallery-splide.is-right', splideConfig('rtl')).mount(window.splide.Extensions);
+}
